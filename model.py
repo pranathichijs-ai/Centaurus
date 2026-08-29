0ded6d5df3198c4439ac400a5778cfa715b4f7de
Angel313-Gyu <bearybeomgyu03@gmail.com>
2026-08-29 22:20:49 +0800

    Refactor model.py for data loading and cleanup
    Updated model.py to include data loading from JSON and removed old training loop.

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+!pip install open_clip_torch
 import open_clip
 
 #Load CLIP
@@ -45,29 +46,17 @@ def run_inference(image_dir, output_path, model):
         
     with open(output_path, "w") as f:
         json.dump(results, f, indent=2)
-        
-#Dataloader that feed images in small batches to the model
+
+!git clone https://github.com/pranathichijs-ai/Centaurus.git
+!ls
+
 from torch.utils.data import DataLoader
 
+
+import json
+with open('/content/Centaurus/aigc-detection3/train_splits.json') as f:
+  data = json.load(f)
+  train_paths = data["train_paths"]
+train_labels = data["train_labels"]
 train_dataset = ImageDataset(train_paths, train_labels)
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-
-#Run through FakeDetector
-for images, labels in train_loader: 
-  print(model(images.cuda())); 
-  break
-
-#Optimizer
-optimizer = torch.optim.Adam(model.head.parameters(), lr=1e-4) #we're only training the head
-loss_fn = nn.BCEWithLogitsLoss() #tells PyTorch that we're doing binary classification. Real(0) vs Fake(1)?
-for epoch in range(5): #go through the entire training dataset 5 times
-    for images, labels in train_loader:#inside each epoch, we take 1 batch of 32 images
-        images, labels = images.cuda(), labels.float().cuda() #move them onto GPU
-        optimizer.zero_grad() #clears prev batch's calculations
-        preds = model(images) #model makes predictions
-        loss = loss_fn(preds, labels) #ask how wrong were those predictions
-        loss.backward() #figures out how the head's parameters contributed to the error
-        optimizer.step() #make changes to the head's parameters to improve future predictions
-    print(f"Epoch {epoch}, loss: {loss.item()}") #the expected output is that as the epoch increases, the loss must decrease
-
-
